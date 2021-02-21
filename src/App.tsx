93ac5665211cb7afe5533df5b37a89c42f22171e
RinkyDewangan <dewangan1707@gmail.com>
2021-02-21 13:46:37 +0530

    First commit for react boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import UserComponent from "components/User";
+
+const App = ():JSX.Element => <UserComponent userName='Peter' profile='Software Developer' isActive={false} />;
+
+export default App;
